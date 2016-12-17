f383f262e233c5037fffaf6e764889450640af27
Kasia Nowak <k.nowak22@o2.pl>
2016-12-17 23:28:00 +0100

    dbset dodane

--- Register/Models/IdentityModels.cs
+++ Register/Models/IdentityModels.cs
@@ -38,6 +38,16 @@ namespace Register.Models
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Parent> Parents { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<MySubject> MySubjects { get; set; }
+        public DbSet<SClass> SClasses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<GradeList> GradeList { get; set; }
+        public DbSet<News> Newses { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Quiz> Quiz { get; set; }
+
+
     }
 
     // ROLE
